520cd289d33d6d48a3bf02dd7805434263a54e0e
AshrafulBadol <davidashraful@gmail.com>
2022-05-23 12:12:08 +0600

    js basic

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ document.write('Number of charectar '+ text.length);*/
 
    //-------Date,Localtime,year-------//
 
-         /*var myDate = new Date();
+         var myDate = new Date();
         
          console.log(myDate.toTimeString());
          console.log(myDate.toLocaleDateString());
@@ -28,16 +28,16 @@ document.write('Number of charectar '+ text.length);*/
          console.log(myDate.getFullYear());
          console.log(myDate.getMinutes());
          console.log(myDate.getHours());
-         console.log(myDate.getTimezoneOffset());*/
+         console.log(myDate.getTimezoneOffset());
 
-         
-          /*var a = ['ashraful','anisha','fahim','amar nam badol','amar nam alamin','amar nam suhag']
+        //slice and splice------
+          var a = ['ashraful','anisha','fahim','amar nam badol','amar nam alamin','amar nam suhag']
            a = a.slice(5,6);
            console.log(a);
 
            var c=[0,1,2,3,4,5,6,7,8,9,]
            c.splice(7,10);
-           console.log(c); */
+           console.log(c); 
            
           
 
@@ -45,14 +45,16 @@ document.write('Number of charectar '+ text.length);*/
             //   let remainde = year % 4;
             //   console.log(remainde==0);
 
-           /* Fibonacci js--------- 
+            //Fibonacci js--------- 
             const fibo = [0,1];
              for(let i=2; i<=30;i++){
                  fibo[i] = fibo[i-1] + fibo[i-2];
             }
-             console.log(fibo);*/
+             console.log(fibo);
 
-            /*  let marks = [23,45,67,94,65,48,13,46,308];
+
+
+             let marks = [23,45,67,94,65,48,13,46,308];
               let max = marks[0];
               for(let i = 0;i<marks.length;i++){
                  var elemant = marks[i];
@@ -71,6 +73,6 @@ document.write('Number of charectar '+ text.length);*/
                 else{
                     console.log('wrong');
                 }
-             }*/
+             }
 
                 
